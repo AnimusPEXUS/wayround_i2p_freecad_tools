5860cbd0ce23ef37eb401b40e79c1210f52aa1e4
AnimusPEXUS <animuspexusagutilities@gmail.com>
2026-04-30 01:59:08 +0300

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name='wayround_i2p_freecad_tools',
+    version='0.1',
+    description='some simple tools to help scripting freecad',
+    url='https://github.com/AnimusPEXUS/wayround_i2p_freecad_tools',
+    packages=[
+        'wayround_i2p.freecad_tools'
+    ]
+)
